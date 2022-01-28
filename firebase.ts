8285179f359c309bc366cb53dd53a70043b981d4
Sha Mwe La <shamwela2002@gmail.com>
2022-01-28 21:17:01 +0630

    Improve

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCsGK6FhMRC1AqUanMY09TTiOxBJe3yMmo',
+  authDomain: 'tasks-firebase-a1a61.firebaseapp.com',
+  projectId: 'tasks-firebase-a1a61',
+  storageBucket: 'tasks-firebase-a1a61.appspot.com',
+  messagingSenderId: '685546362381',
+  appId: '1:685546362381:web:e6dc54eb0fda4aa71fe744',
+}
